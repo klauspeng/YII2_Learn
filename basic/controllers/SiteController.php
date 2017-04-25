a62f821812fc888df26074d37bcad73e963c1783
klaus <2511634167@qq.com>
2017-04-25 14:15:03 +0800

    0425

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -122,4 +122,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    //第一次问候
+    public function actionSay($message = 'Hello')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
 }
