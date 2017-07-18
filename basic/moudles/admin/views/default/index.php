99439426c7da309a8852dacede41140cce8c36e1
klauspeng <2511634167@qq.com>
2017-07-18 13:51:36 +0800

    后台整合 startbootstrap-sb-admin-2-3.3.7-1

--- basic/moudles/admin/views/default/index.php
+++ basic/moudles/admin/views/default/index.php
@@ -1,1 +1,5 @@
-adasd
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">首页</h1>
+    </div>
+</div>
--- basic/moudles/blog/controllers/IndexController.php
+++ basic/moudles/blog/controllers/IndexController.php
@@ -17,4 +17,19 @@ class IndexController extends BaseController
     {
         return $this->render('index');
     }
+
+    public function actionTest()
+    {
+        // $uid = md5(uniqid(md5(microtime(true)), true));
+        // echo $uid;
+        // var_dump(strlen($uid));
+
+        // 获取ID为 "forum" 的模块
+        $module = \Yii::$app->getModule('blog');
+
+        var_dump($module);
+
+
+        $module = \Yii::$app->controller->module;
+    }
 }
